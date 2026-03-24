5f9e78f68b66d5949db9698bcadf2696fde5de77
Utkarsh-2487651 <2487651@cognizant.com>
2026-03-24 17:56:27 +0530

    Initial setup for project Done - Jai Mata Di

---
+++ ClaimAuto.HealthSystems/ClaimAuto.HealthSystems.Server/Models/PolicyTable.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ClaimAuto.HealthSystems.Server.Models
+{
+    public class PolicyTable
+    {
+        [Key] public int PolicyId { get; set; }
+        [Required] public string PolicyName { get; set; }
+        [Column(TypeName = "decimal(18,2)")] public decimal CoverageLimit { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")] public decimal Deductible { get; set; }
+        public decimal CoPayPercentage { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ ClaimAuto.HealthSystems/ClaimAuto.HealthSystems.Server/Models/UserTable.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClaimAuto.HealthSystems.Server.Models
+{
+    public class UserTable
+    {
+        [Key] public int UserId { get; set; }
+        [Required] public string  FullName { get; set; }
+        [Required] public string  Email { get; set; }
+        [Required] public string UserRole { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
