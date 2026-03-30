6e5dbfa4ee58eadf4d1ad97e928b62256ab31de3
Utkarsh-2487651 <2487651@cognizant.com>
2026-03-30 15:21:58 +0530

    Remove all tables from models

--- ClaimAuto.HealthSystems.Server/Data/ApplicationDbContext.cs
+++ ClaimAuto.HealthSystems.Server/Data/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
-﻿using ClaimAuto.HealthSystems.Server.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace ClaimAuto.HealthSystems.Server.Data
 {
@@ -7,12 +6,6 @@ namespace ClaimAuto.HealthSystems.Server.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         { }
-        public DbSet<UserTable> Users { get; set; }
-        public DbSet<PolicyTable> Policies { get; set; }
-        public DbSet<ClaimTable> Claims { get; set; }
-        public DbSet<UserPolicyTable> UserPolicies { get; set; }
-        public DbSet<ProviderTable> Providers { get; set; }
-        public DbSet<ClaimPolicyTable> ClaimPolicies { get; set; }
-        public DbSet<AutomationRuleTable> AutomationRules { get; set; }
+       
     }
 }
