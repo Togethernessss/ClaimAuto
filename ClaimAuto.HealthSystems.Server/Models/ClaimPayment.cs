cc5285d78a5eed16d1e8490279f516f0f9675fa0
Mall <2487427@cognizant.com>
2026-03-31 15:07:50 +0530

    updated ClaimPayment table

--- ClaimAuto.HealthSystems.Server/Models/ClaimPayment.cs
+++ ClaimAuto.HealthSystems.Server/Models/ClaimPayment.cs
@@ -1,6 +1,20 @@
-﻿namespace ClaimAuto.HealthSystems.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ClaimAuto.HealthSystems.Server.Models;
+
+public class Claim
 {
-    public class ClaimPayment
-    {
-    }
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public decimal PaidAmount { get; set; }
+    public string PaymentMode { get; set; }
+    public string PaymentStatus { get; set; }
+    public DateTime PaidAt { get; set; }
+
+    public int ProcessedBy { get; set; }
+
+    [ForeignKey(nameof(ProcessedBy))]
+    public ApplicationUser ProcessedByUser { get; set; }
 }
