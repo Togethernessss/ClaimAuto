b970277bf12e85148de6eedafc4d38054f4d762b
Aayush <2487674@cognizant.com>
2026-03-31 15:14:49 +0530

    ClaimStatusHistory Defined

--- ClaimAuto.HealthSystems.Server/Models/ClaimStatusHistory.cs
+++ ClaimAuto.HealthSystems.Server/Models/ClaimStatusHistory.cs
@@ -1,6 +1,17 @@
-﻿namespace ClaimAuto.HealthSystems.Server.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClaimAuto.HealthSystems.Server.Models
 {
     public class ClaimStatusHistory
     {
+        public int Id { get; set; } 
+        public int ClaimId { get; set; }
+        [ForeignKey("Claim")]
+        public Claim Claim { get; set; }
+        public string OldStatus {  get; set; }
+        public string NewStatus { get; set; }
+        public int ChangedBy { get; set; }
+        public string Remarks { get; set; }
+        public DateTime ChangedAt { get; set; }
     }
 }
