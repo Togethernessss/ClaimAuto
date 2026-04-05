532381565e49ef6983c9796156f9751503ca6cf9
Saxena <2487413@cognizant.com>
2026-04-05 12:02:18 +0530

    DB created which having 22 tables with one Enums file and DbContext setted up

---
+++ ClaimAuto.HealthSystems.Server/Model/EligibilityCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClaimAuto.HealthSystems.Server.Model
+{
+    [Table("EligibilityChecks")]
+    public class EligibilityCheck
+    {
+        [Key]
+        public int CheckID { get; set; }
+
+        [ForeignKey("Member")]
+        public int MemberID { get; set; }
+        public Member Member { get; set; } = null!;
+
+        [ForeignKey("Policy")]
+        public int PolicyID { get; set; }
+        public Policy Policy { get; set; } = null!;
+
+        [Required]
+        public DateTime CheckedAt { get; set; } = DateTime.UtcNow;
+
+        [MaxLength(100)]
+        public string? Source { get; set; }
+
+        public string? ResultJSON { get; set; }
+
+        public int? TTL { get; set; }
+
+        public int? PerformedByID { get; set; }
+        [ForeignKey("PerformedByID")]
+        public User? PerformedBy { get; set; }
+    }
+}
---
+++ ClaimAuto.HealthSystems.Server/Model/Policy.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClaimAuto.HealthSystems.Server.Model
+{
+    [Table("Policies")]
+    public class Policy
+    {
+        [Key]
+        public int PolicyID { get; set; }
+
+        [MaxLength(50)]
+        public string? PlanCode { get; set; }
+
+        [Required, MaxLength(150)]
+        public string PlanName { get; set; } = string.Empty;
+
+        public string? CoverageRulesJSON { get; set; }
+
+        [Column(TypeName = "decimal(12,2)")]
+        public decimal? DeductibleAmount { get; set; }
+
+        [Column(TypeName = "decimal(12,2)")]
+        public decimal? OutOfPocketMax { get; set; }
+
+        [Required]
+        public DateTime EffectiveFrom { get; set; }
+
+        public DateTime? EffectiveTo { get; set; }
+
+        [Required]
+        public PolicyStatus Status { get; set; } = PolicyStatus.Active;
+
+        // Navigation
+        public ICollection<Member> Members { get; set; } = new List<Member>();
+        public ICollection<Claim> Claims { get; set; } = new List<Claim>();
+        public ICollection<EligibilityCheck> EligibilityChecks { get; set; } = new List<EligibilityCheck>();
+    }
+}
