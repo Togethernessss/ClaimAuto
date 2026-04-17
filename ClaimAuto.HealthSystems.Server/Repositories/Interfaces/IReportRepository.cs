1065dc725dc24579c9bc5eade352abc101cef18b
Aayush <2487674@cognizant.com>
2026-04-17 14:21:19 +0530

    DTO mappings corrected from controllers to repos

--- ClaimAuto.HealthSystems.Server/Repositories/Interfaces/IReportRepository.cs
+++ ClaimAuto.HealthSystems.Server/Repositories/Interfaces/IReportRepository.cs
@@ -1,28 +1,26 @@
 ﻿using ClaimAuto.HealthSystems.Server.DTOs;
-using ClaimAuto.HealthSystems.Server.Model;
 
 namespace ClaimAuto.HealthSystems.Server.Repositories.Interfaces
 {
     public interface IReportRepository
     {
-        Task<List<Report>> GetAllReportsAsync(string? scope);
+        Task<List<ReportResponseDto>> GetAllReportsAsync(string? scope);
 
-        Task<Report?> GetReportByIdAsync(int id);
+        Task<ReportResponseDto?> GetReportByIdAsync(int id);
 
-        // Reads from Claims, Payments, AdjudicationRecords
-        Task<Report> GenerateReportAsync(
+        Task<ReportResponseDto> GenerateReportAsync(
             GenerateReportDto dto,
             int generatedById);
 
-        Task<List<KPI>> GetAllKPIsAsync();
+        Task<List<KPIResponseDto>> GetAllKPIsAsync();
 
-        Task<KPI?> UpdateKPIAsync(
+        Task<KPIResponseDto?> UpdateKPIAsync(
             int id,
             UpdateKPIDto dto);
 
-        Task<List<AuditPackage>> GetAllAuditPackagesAsync();
+        Task<List<AuditPackageResponseDto>> GetAllAuditPackagesAsync();
 
-        Task<AuditPackage> GenerateAuditPackageAsync(
+        Task<AuditPackageResponseDto> GenerateAuditPackageAsync(
             DateTime periodStart,
             DateTime periodEnd,
             int generatedById);
