8edcb69d9627faa6121342e964ebe3c3d11e08a6
Saxena <2487413@cognizant.com>
2026-05-12 17:24:13 +0530

    Remove main.tsx and app.tsx

--- ClaimAuto.HealthSystems.Server/Controllers/PoliciesController.cs
+++ ClaimAuto.HealthSystems.Server/Controllers/PoliciesController.cs
@@ -32,7 +32,7 @@ namespace ClaimAuto.HealthSystems.Server.Controllers
         /// <summary>Returns all policies regardless of status.</summary>
         /// <response code="200">Returns list of all policies.</response>
         [HttpGet]
-        [Authorize(Roles = "Admin,InsuranceStaff")]
+        [Authorize(Roles = "Admin,InsuranceStaff,Hospital")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> GetAllPolicies()
         {
